573406e09d53da6169235ac1bb169c8225ac0e16
Eamonn O'Brien-Strain <eobrain@gmail.com>
2019-10-28 22:01:07 -0700

    Refactor out common code between index.js and debug.js

--- site/debug.js
+++ site/debug.js
@@ -1,79 +1,34 @@
-/* global mergiWords, localStorage */
+/* global localStorage */
 
-const lang = 'es'
-const country = 'mx'
+(() => {
+  const { newCards, readCards, images, merge, score } = window.common
 
-const queries = [] // the phrases
-const images = {} // list of images for each phrase
-mergiWords.forEach((word) => {
-  if (word.lang === lang && word.country === country) {
-    queries.push(word.query)
-    images[word.query] = word.images
-  }
-})
+  document.body.onload = () => {
+    const tableEl = document.getElementById('table')
 
-const merge = (existing, added) => {
-  const key = (card) => `${card.query}|${card.reversed}`
-  const result = []
-  const included = {}
-  const hasImages = (card) => !!images[card.query]
-  if (existing) {
-    existing.filter(hasImages).forEach((card) => {
-      result.push(card)
-      included[key(card)] = true
-    })
-  }
-  if (added) {
-    added.filter(hasImages).forEach((card) => {
-      const k = key(card)
-      if (!included[k]) {
-        result.push(card)
-        included[k] = true
+    const sort = () => {
+      cards.sort((a, b) => a.score - b.score)
+    }
+
+    const cards = merge(readCards(), newCards())
+    sort()
+
+    cards.forEach((card) => {
+      let imgHtml = ''
+      const imagesOfQuery = images[card.query]
+      if (imagesOfQuery) {
+        imagesOfQuery.forEach((image) => {
+          imgHtml += `<img src="${image.src}" width="${image.width}" height="${image.height}"/>`
+        })
+      } else {
+        imgHtml = '(No images)'
       }
+
+      const query = card.reversed ? card.query : `<strong>${card.query}</strong>`
+      const responsesString = JSON.stringify(card.responses.map((r) => `${r.correctness}`))
+      tableEl.insertAdjacentHTML('beforeend',
+            `<tr><td>${score(card.responses)}</td><td>${responsesString}</td><td>${query}</td><td>${imgHtml}</td></tr>`
+      )
     })
   }
-  return result
-}
-
-document.body.onload = () => {
-  const tableEl = document.getElementById('table')
-
-  // Return ordered list of cards
-  const newOrder = () => {
-    const cards = []
-    let score = 0
-    for (let i = 0; i < 2; ++i) {
-      const reversed = (i === 1)
-      queries.forEach((query) => {
-        ++score
-        cards.push({ query, reversed, score })
-      })
-    }
-    return cards
-  }
-
-  const sort = () => {
-    order.sort((a, b) => a.score - b.score)
-  }
-
-  const KEY = 'mergi-order'
-  const order = merge(JSON.parse(localStorage.getItem(KEY)), newOrder())
-  sort()
-
-  order.forEach((card) => {
-    let imgHtml = ''
-    const imagesOfQuery = images[card.query]
-    if (imagesOfQuery) {
-      imagesOfQuery.forEach((image) => {
-        imgHtml += `<img src="${image.src}" width="${image.width}" height="${image.height}"/>`
-      })
-    } else {
-      imgHtml = '(No images)'
-    }
-
-    const query = card.reversed ? card.query : `<strong>${card.query}</strong>`
-    tableEl.insertAdjacentHTML('beforeend',
-            `<tr><td>${card.score}</td><td>${query}</td><td>${imgHtml}</td></tr>`
-    )
-  })
-}
+})()
--- site/index.js
+++ site/index.js
@@ -1,144 +1,85 @@
-/* global mergiWords, localStorage */
 
-const lang = 'es'
-const country = 'mx'
+(() => {
+  const { newCards, readCards, writeCards, images, merge, score } = window.common
 
-const queries = [] // the phrases
-const images = {} // list of images for each phrase
-mergiWords.forEach((word) => {
-  if (word.lang === lang && word.country === country) {
-    queries.push(word.query)
-    images[word.query] = word.images
-  }
-})
-
-const merge = (existing, added) => {
-  const key = (card) => `${card.query}|${card.reversed}`
-  const result = []
-  const included = {}
-  const hasImages = (card) => !!images[card.query]
-  if (existing) {
-    existing.filter(hasImages).forEach((card) => {
-      result.push(card)
-      included[key(card)] = true
-    })
-  }
-  if (added) {
-    added.filter(hasImages).forEach((card) => {
-      const k = key(card)
-      if (!included[k]) {
-        result.push(card)
-        included[k] = true
-      }
-    })
-  }
-  return result
-}
-
-const TAO = 1000.0 * 60 * 60 * 24
-
-const score = (responses) => {
-  if (responses.length === 0) {
-    return -1
-  }
-  const now = Date.now()
-  const weightedSum = responses
-    .map((response) => response.correctness * Math.exp((response.t - now) / TAO))
-    .reduce((sum, x) => sum + x)
-  return weightedSum / responses.length
-}
-
-const order = (() => {
-  // Return ordered list of cards
-  const newOrder = () => {
-    const cards = []
-    for (let i = 0; i < 2; ++i) {
-      const reversed = (i === 1)
-      queries.forEach((query) => {
-        const responses = []
-        cards.push({ query, reversed, responses })
-      })
+  const order = (() => {
+    const sort = () => {
+      cards.sort((a, b) => score(a.responses) - score(b.responses))
     }
-    return cards
-  }
 
-  const sort = () => {
-    order.sort((a, b) => score(a.responses) - score(b.responses))
-  }
+    const head = () => cards[0]
 
-  const head = () => order[0]
+    const update = (correctness) => {
+      const t = Date.now()
+      cards[0].responses.push({ t, correctness })
+      sort()
+      writeCards(cards)
+    }
 
-  const update = (correctness) => {
-    const t = Date.now()
-    order[0].responses.push({ t, correctness })
+    const cards = merge(readCards(), newCards())
     sort()
-    localStorage.setItem(KEY, JSON.stringify(order))
+
+    return { head, update }
+  })()
+
+  const wordEl = document.getElementById('word')
+  const imagesEl = document.getElementById('images')
+  const correctEl = document.getElementById('correct')
+  const mehEl = document.getElementById('meh')
+  const wrongEl = document.getElementById('wrong')
+
+  const ask = () => {
+    const { query, reversed } = order.head()
+
+    correctEl.style.display = 'none'
+    mehEl.style.display = 'none'
+    wrongEl.style.display = 'none'
+    if (reversed) {
+      wordEl.classList.add('back')
+      imagesEl.classList.add('front')
+    } else {
+      wordEl.classList.add('front')
+      imagesEl.classList.add('back')
+    }
+    wordEl.innerHTML = query
+    while (imagesEl.firstChild) {
+      imagesEl.removeChild(imagesEl.firstChild)
+    }
+    images[query].forEach((image) => {
+      const imgEl = document.createElement('img')
+      imgEl.src = image.src
+      imgEl.width = image.width
+      imgEl.height = image.height
+      imagesEl.append(imgEl)
+    })
   }
 
-  const KEY = 'mergi-order-v3'
-  const order = merge(JSON.parse(localStorage.getItem(KEY)), newOrder())
-  sort()
+  imagesEl.onclick = () => {
+    wordEl.classList.remove('back')
+    correctEl.style.display = 'inline'
+    mehEl.style.display = 'inline'
+    wrongEl.style.display = 'inline'
+  }
 
-  return { head, update }
+  wordEl.onclick = () => {
+    imagesEl.classList.remove('back')
+    correctEl.style.display = 'inline'
+    mehEl.style.display = 'inline'
+    wrongEl.style.display = 'inline'
+  }
+
+  correctEl.onclick = () => {
+    order.update(1.0)
+    ask()
+  }
+  mehEl.onclick = () => {
+    order.update(0.5)
+    ask()
+  }
+  wrongEl.onclick = () => {
+    order.update(0.0)
+    ask()
+  }
+
+  document.body.onload = ask
 })()
-
-const wordEl = document.getElementById('word')
-const imagesEl = document.getElementById('images')
-const correctEl = document.getElementById('correct')
-const mehEl = document.getElementById('meh')
-const wrongEl = document.getElementById('wrong')
-
-const ask = () => {
-  const { query, reversed } = order.head()
-
-  correctEl.style.display = 'none'
-  mehEl.style.display = 'none'
-  wrongEl.style.display = 'none'
-  if (reversed) {
-    wordEl.classList.add('back')
-    imagesEl.classList.add('front')
-  } else {
-    wordEl.classList.add('front')
-    imagesEl.classList.add('back')
-  }
-  wordEl.innerHTML = query
-  while (imagesEl.firstChild) {
-    imagesEl.removeChild(imagesEl.firstChild)
-  }
-  images[query].forEach((image) => {
-    const imgEl = document.createElement('img')
-    imgEl.src = image.src
-    imgEl.width = image.width
-    imgEl.height = image.height
-    imagesEl.append(imgEl)
-  })
-}
-
-imagesEl.onclick = () => {
-  wordEl.classList.remove('back')
-  correctEl.style.display = 'inline'
-  mehEl.style.display = 'inline'
-  wrongEl.style.display = 'inline'
-}
-
-wordEl.onclick = () => {
-  imagesEl.classList.remove('back')
-  correctEl.style.display = 'inline'
-  mehEl.style.display = 'inline'
-  wrongEl.style.display = 'inline'
-}
-
-correctEl.onclick = () => {
-  order.update(1.0)
-  ask()
-}
-mehEl.onclick = () => {
-  order.update(0.5)
-  ask()
-}
-wrongEl.onclick = () => {
-  order.update(0.0)
-  ask()
-}
-
-document.body.onload = ask
