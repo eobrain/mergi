431e13fe2ca7812d9eb1f35939f42a5a730a0069
Eamonn O'Brien-Strain <eobrain@gmail.com>
2019-11-13 22:36:53 -0800

    Add page showing overview of whole page.

---
+++ site/deck.js
@@ -0,0 +1,32 @@
+// Copyright (c) 2019 Eamonn O'Brien-Strain All rights reserved. This
+// program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v1.0 which accompanies this
+// distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+
+import { newCards, readCards, merge, score, decay } from './common.js'
+
+document.body.onload = () => {
+  const deckEl = document.getElementById('deck')
+
+  const sort = () => {
+    cards.sort((a, b) => score(a) - score(b))
+  }
+
+  const cards = merge(readCards(), newCards())
+  sort()
+
+  cards.forEach((card) => {
+    const cardEl = document.createElement('li')
+    if (card.reversed) {
+      cardEl.className = 'reversed'
+    }
+    card.responses.forEach((r) => {
+      const rEl = document.createElement('div')
+      const color = `hsl(${120 * r.correctness}, ${100 * decay(r.t)}%, 50%)`
+      rEl.style.background = color
+      cardEl.append(rEl)
+    })
+    deckEl.append(cardEl)
+  })
+}
