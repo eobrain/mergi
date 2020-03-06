908aa96fa1b3678e0dc98d22514c8eb87ba0d628
Eamonn O'Brien-Strain <eobrain@gmail.com>
2020-03-05 22:29:47 -0800

    Take advantage of new array syntax to may build file more DRY.

--- build.js
+++ build.js
@@ -12,6 +12,8 @@ import { DATA } from './node/config.js'
 import { basename } from 'path'
 import build from 'bajel'
 
+// const trace = x => console.trace(x) || x
+
 const COMPILEJS = 'java -jar tools/closure/closure-compiler-v20191111.jar -O ADVANCED --externs src/js/externs.js'
 const LOCALES = ['en_ie', 'en_us', 'es_es', 'es_mx', 'fr_fr']
 const STYLE = ['app', 'common', 'debug', 'deck', 'index', 'info', 'normalize']
@@ -20,6 +22,23 @@ const PARTIALS = ['src/html/head.mustache', 'src/html/footer.mustache', 'src/htm
 const CARDJS = ['search_url.js', 'main.js', 'shared.js', 'search_url.js', 'card.js', 'storage.js', 'word.js']
 const DECKJS = ['deck.js', 'shared.js', 'search_url.js', 'card.js', 'storage.js', 'word.js']
 const DEBUGJS = ['debug.js', 'shared.js', 'search_url.js', 'card.js', 'storage.js', 'word.js']
+
+const mustache = (page, localePage) => [
+  `src/json/${localePage}.json`,
+  `src/html/${page}.html`,
+  ...PARTIALS,
+  c => [MUSTACHE, `src/json/${localePage}.json src/html/${page}.html >`, c.target]
+]
+
+const compilejs = (module, js) => [
+  ...js.map(x => `src/js/${x}`),
+  'src/js/externs.js',
+  c => [COMPILEJS,
+    `--create_source_map site/${module}_compiled.map --js_output_file`,
+    c.target,
+    c.source],
+  c => ['echo', `'//# sourceMappingURL=/${module}_compiled.map'`, '>>', c.target]
+]
 
 build({
   compiled: [
@@ -36,8 +55,9 @@ build({
 
   runtest: [c => 'npm test'],
 
-  runbrowsertest: ['lint',
-    c => console.log('Open http://localhost:8887/browsertest'),
+  runbrowsertest: [
+    'lint',
+    c => ': Open http://localhost:8887/browsertest',
     c => 'python -m SimpleHTTPServer 8887'
   ],
 
@@ -53,68 +73,29 @@ build({
     'site/privacy.html'
   ],
 
-  'site/card_%.html': ['src/json/card_%.json', 'src/html/card.html', ...PARTIALS,
-    c => `${MUSTACHE} src/json/card_%.json src/html/card.html >${c.target}`
-  ],
-  'site/deck_%.html': ['src/json/deck_%.json', 'src/html/deck.html', ...PARTIALS,
-    c => `${MUSTACHE} src/json/deck_%.json src/html/deck.html >${c.target}`
-  ],
-  'site/debug_%.html': ['src/json/debug_%.json', 'src/html/debug.html', ...PARTIALS,
-    c => `${MUSTACHE} src/json/debug_%.json src/html/debug.html >${c.target}`
-  ],
-  'site/credit.html': ['src/json/credit.json', 'src/html/credit.html', ...PARTIALS,
-    c => `${MUSTACHE} src/json/credit.json src/html/credit.html >${c.target}`
-  ],
-  'site/index.html': ['src/json/index.json', 'src/html/index.html', ...PARTIALS,
-    c => `${MUSTACHE} src/json/index.json src/html/index.html >${c.target}`
-  ],
-  'site/info.html': ['src/json/info.json', 'src/html/info.html', ...PARTIALS,
-    c => `${MUSTACHE} src/json/info.json src/html/info.html >${c.target}`
-  ],
-  'site/privacy.html': ['src/html/privacy.html', 'src/json/privacy.json', ...PARTIALS,
-    c => `${MUSTACHE} src/json/privacy.json src/html/privacy.html >${c.target}`
+  'site/card_%.html': mustache('card', 'card_%'),
+  'site/deck_%.html': mustache('deck', 'deck_%'),
+  'site/debug_%.html': mustache('debug', 'debug_%'),
+  'site/info.html': mustache('info', 'info'),
+  'site/index.html': mustache('index', 'index'),
+  'site/credit.html': mustache('credit', 'credit'),
+  'site/privacy.html': mustache('privacy', 'privacy'),
+
+  'site/card_%_compiled.js': compilejs('card_%s', ['words_%.js', 'card_%.js', ...CARDJS]),
+  'site/deck_%_compiled.js': compilejs('deck_%s', ['words_%.js', 'deck_%.js', ...DECKJS]),
+  'site/debug_%_compiled.js': compilejs('debug_%s', ['words_%.js', 'debug_%.js', ...DEBUGJS]),
+  'site/index_compiled.js': compilejs('card', ['index.js']),
+
+  'site/%.js': [
+    'src/js/%.js',
+    c => [
+      `npx terser --module --ecma 6 --compress --mangle --source-map "base='site',url='${basename(c.target)}.map'" --output`,
+      c.target, '--', c.source
+    ]
   ],
 
-  'site/card_%_compiled.js': [
-    'src/js/words_%.js',
-    'src/js/card_%.js',
-    ...CARDJS.map(x => `src/js/${x}`),
-    'src/js/externs.js',
-    c => `
-      ${COMPILEJS} --create_source_map site/app_%_compiled.map --js_output_file ${c.target} ${c.source}
-      echo '//# sourceMappingURL=/app_%_compiled.map' >> ${c.target}
-    `
-  ],
-  'site/deck_%_compiled.js': [
-    'src/js/words_%.js',
-    'src/js/deck_%.js',
-    ...DECKJS.map(x => `src/js/${x}`), 'src/js/externs.js',
-    c => `
-      ${COMPILEJS} --create_source_map site/deck_%_compiled.map --js_output_file ${c.target} ${c.source}
-      echo '//# sourceMappingURL=/deck_%_compiled.map' >> ${c.target}
-    `
-  ],
-  'site/debug_%_compiled.js': [
-    'src/js/words_%.js',
-    'src/js/debug_%.js', ...DEBUGJS.map(x => `src/js/${x}`), 'src/js/externs.js',
-    c => `
-      ${COMPILEJS} --create_source_map site/debug_%_compiled.map --js_output_file ${c.target} ${c.source}
-      echo '//# sourceMappingURL=/debug_%_compiled.map' >> ${c.target}
-    `
-  ],
-  'site/index_compiled.js': ['src/js/index.js', 'src/js/externs.js',
-    c => `
-      ${COMPILEJS} --create_source_map site/index_compiled.map --js_output_file ${c.target} ${c.source}
-      echo '//# sourceMappingURL=/index_compiled.map' >> ${c.target}
-    `
-  ],
-
-  'site/%.js': ['src/js/%.js',
-    c =>
-      `npx terser --module --ecma 6 --compress --mangle --source-map "base='site',url='${basename(c.target)}.map'" --output ${c.target} -- ${c.source}`
-  ],
-
-  'site/css/%.css': ['src/scss/%.scss',
+  'site/css/%.css': [
+    'src/scss/%.scss',
     c => `
       mkdir -p site/css
       npx node-sass --output-style compressed src/scss/%.scss > site/css/%.css
@@ -137,7 +118,11 @@ build({
     'site/search_url.js',
     'site/shared.js'],
 
-  words: ['lint', DATA, 'node/fetch_words.js', 'node/scrape.js',
+  words: [
+    'lint',
+    DATA,
+    'node/fetch_words.js',
+    'node/scrape.js',
     c => `
       node node/fetch_words.js
       npx standard --fix src/js/words_??_??.js
@@ -146,8 +131,11 @@ build({
 
   'ocr-test': ['site/ocr.html'],
 
-  'site/ocr.html': ['site/words_es_mx.js', 'node/ocr.js', 'node/ocr_test.js',
-    c => `node node/ocr_test.js > ${c.target}`
+  'site/ocr.html': [
+    'site/words_es_mx.js',
+    'node/ocr.js',
+    'node/ocr_test.js',
+    c => ['node node/ocr_test.js >', c.target]
   ],
 
   lint: [
@@ -163,13 +151,17 @@ build({
   `
   ],
 
-  serve: ['compiled',
+  serve: [
+    'compiled',
     c => `
       : visit https://localhost:8888
       cd site && python -m SimpleHTTPServer 8888
     `],
 
-  'serve-ssl': ['site/server.pem', 'site/serve.py', 'compiled',
+  'serve-ssl': [
+    'site/server.pem',
+    'site/serve.py',
+    'compiled',
     c => `
       : visit https://localhost:4443
       cd site && python serve.py
